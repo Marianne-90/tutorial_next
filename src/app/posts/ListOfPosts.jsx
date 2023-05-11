37f73312358e8cc4c30fadc9382f8c44f176f2bc
Marianne Garrido <marianne.garridom@gmail.com>
2023-05-11 13:45:51 -0600

    añadi la lista de post en un componente apartes use un use client en el like button y puse un fetch que espera en el list of post

---
+++ src/app/posts/ListOfPosts.jsx
@@ -0,0 +1,25 @@
+import { LikeButton } from "./LikeButton";
+
+const fetchPost = () => {
+  //*! esto es para que se actualice cada cierto tiempo
+  return fetch("https://jsonplaceholder.typicode.com/posts", {
+    next: { revalidate: 60 },
+  }).then((res) => res.json());
+
+  //*! esto es para que sea dinámico si no se hace un html congelado
+  //   return fetch("https://jsonplaceholder.typicode.com/posts",{cache: "no-store"}).then((res) =>
+  //     res.json()
+  //   );
+};
+
+export async function ListOfPost() {
+  const posts = await fetchPost();
+
+  return posts.slice(0, 5).map((post) => (
+    <article key={post.id}>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <LikeButton id={post.id} />
+    </article>
+  ));
+}
--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,23 +1,10 @@
-
-const fetchPost = () => {
-  return fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
-    res.json()
-  );
-};
+import { ListOfPost } from "./ListOfPosts";
 
 export default async function PostPage() {
 
-  const posts = await fetchPost();
-
-
   return (
     <section>
-      {posts.slice(0,5).map((post) => (
-        <article key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-        </article>
-      ))}
+      <ListOfPost/>
     </section>
   );
 }
